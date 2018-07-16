3c0ad7c565deed76663bd5f434b36f99618b3d79
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-07-16 16:51:58 +0300

    improved example

--- examples/wasm-runner.rs
+++ examples/wasm-runner.rs
@@ -14,17 +14,25 @@
 // http://localhost:8000/wasm-basic.html should output "6" in console
 
 fn main() {
+    let path = std::path::Path::new("target/wasm32-unknown-unknown/debug/examples");
+    let result_html_path = {
+        let mut temp = std::path::PathBuf::from(path);
+        temp.push("wasm-basic.html");
+        temp
+    };
+
     let _ = std::process::Command::new("cargo")
         .args(&["build", "--target=wasm32-unknown-unknown", "--example", "wasm-basic"])
         .output().unwrap();
     let _ = std::process::Command::new("wasm-gc")
-        .current_dir("target/wasm32-unknown-unknown/debug/examples")
+        .current_dir(path)
         .args(&["wasm-basic.wasm", "stripped.wasm"])
         .output().unwrap();
-    let _ = std::fs::copy("examples/wasm-basic.html", "target/wasm32-unknown-unknown/debug/examples/wasm-basic.html")
+    let _ = std::fs::copy("examples/wasm-basic.html", result_html_path)
         .unwrap();
+    println!("visit http://localhost:8000/wasm-basic.html, press ctrl+c to quit");
     let _ = std::process::Command::new("python3")
-        .current_dir("target/wasm32-unknown-unknown/debug/examples")
+        .current_dir(path)
         .args(&["-m", "http.server", "8000"])
         .output().unwrap();
 }
