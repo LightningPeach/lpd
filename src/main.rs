406c3bd6155beb7fb32a59e50c87543f02176965
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-07-30 21:01:15 +0300

    added ping/pong

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use serde::ser::Serialize;
 use std::net::{TcpStream, SocketAddr};
 
 use brontide::tcp_communication::{Stream, NetAddress};
-use wire::{Message, BinarySD, Init, RawFeatureVector};
+use wire::{Message, BinarySD, Init, RawFeatureVector, FeatureBit, Ping, Pong};
 
 fn main() {
 //    let local_priv_bytes: [u8; SECRET_KEY_SIZE] = rand::random();
@@ -31,7 +31,7 @@ fn main() {
     let local_priv_bytes: [u8; SECRET_KEY_SIZE] = rand::random();
     let local_priv = SecretKey::from_slice(&Secp256k1::new(), &local_priv_bytes).unwrap();
 
-    let remote_pub_hex = "03f09034d318698ae6ec5e66b61b06ee5b2789fd3bce1cd62da1ba954de002a785";
+    let remote_pub_hex = "0321a4a0c88e3bd2b757972eeda45673f624a504d7e14eb86c23922706668711e4";
     let remote_pub_bytes = hex::decode(remote_pub_hex).unwrap();
     let remote_pub = PublicKey::from_slice(&Secp256k1::new(), &remote_pub_bytes).unwrap();
 
@@ -46,12 +46,18 @@ fn main() {
 
     println!("dial: OK");
 
-    let global_features = RawFeatureVector::new();
-    let local_features = RawFeatureVector::new();
-    let init_msg_req = Init::new(global_features, local_features);
+    let init_msg_req = {
+        use self::FeatureBit::*;
+
+        let global_features = RawFeatureVector::new();
+        let local_features = RawFeatureVector::new().set_bit(InitialRoutingSync);
+        let init = Init::new(global_features, local_features);
+        Message::Init(init)
+    };
 
     let mut data = Vec::<u8>::new();
     BinarySD::serialize(&mut data, &init_msg_req).unwrap();
+    println!("init_req: {:?}", data);
 
     brontide_stream.encrypt_and_write_message(&data).unwrap();
     println!("init_req: OK");
@@ -64,4 +70,25 @@ fn main() {
 
     let init_msg_resp: Message = BinarySD::deserialize(&raw_init_msg_resp[..]).unwrap();
     println!("{:?}", init_msg_resp);
+
+    let mut pong = |pong_length: u16| {
+        let ping = Ping::new(16, pong_length)?;
+        let mut data = Vec::<u8>::new();
+        BinarySD::serialize(&mut data, &Message::Ping(ping)).map_err(|_| ())?;
+        brontide_stream.encrypt_and_write_message(&data).map_err(|_| ())?;
+        data = brontide_stream.read_and_decrypt_message().map_err(|_| ())?;
+        match BinarySD::deserialize(&data[..]).map_err(|_| ())? {
+            Message::Pong(pong) => {
+                if pong.length() == pong_length {
+                    Ok(pong)
+                } else {
+                    Err(())
+                }
+            },
+            _ => Err(())
+        }
+    };
+
+    let q = (0..8).into_iter().map(|i| pong(16 + i).unwrap()).collect::<Vec<_>>();
+    println!("{:?}", q)
 }
