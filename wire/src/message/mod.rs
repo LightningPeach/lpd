45c7b5439d9a52c95db4eb18e78f68df5548dea4
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-08-10 11:37:34 +0300

    added convenience methods to Message

--- wire/src/message/mod.rs
+++ wire/src/message/mod.rs
@@ -21,7 +21,7 @@ use std::u16;
 pub type MessageSize = u16;
 
 macro_rules! message {
-    (pub enum $name:ident { $($variant:ident($rtt:expr)),* }) => {
+    (pub enum $name:ident { $($variant:ident($rtt:expr, $unwrap_method:ident)),* }) => {
         /// Tagged union, the variant name equals to the type name witch the variant contains
         #[derive(Eq, PartialEq, Debug)]
         pub enum $name {
@@ -81,6 +81,16 @@ macro_rules! message {
                     )*
                 }
             }
+
+            $(
+                pub fn $unwrap_method(self) -> Option<$variant> {
+                    use self::$name::*;
+                    match self {
+                        $variant(t) => Some(t),
+                        _ => None,
+                    }
+                }
+            )*
         }
     }
 }
@@ -90,25 +100,25 @@ macro_rules! message {
 /// Implements `Eq`, `Debug`, `Serialize`, `Deserialize`
 message! {
     pub enum Message {
-        Init(16u16),
-        Error(17u16),
-        Ping(18u16),
-        Pong(19u16),
-        OpenChannel(32u16),
-        AcceptChannel(33u16),
-        FundingCreated(34u16),
-        FundingSigned(35u16),
-        FundingLocked(36u16),
-        ShutdownChannel(38u16),
-        ClosingNegotiation(39u16),
-        UpdateAddHtlc(128u16),
-        UpdateFulfillHtlc(130u16),
-        UpdateFailHtlc(131u16),
-        UpdateFailMalformedHtlc(135u16),
-        CommitmentSigned(132u16),
-        RevokeAndAck(133u16),
-        UpdateFee(134u16),
-        ReestablishChannel(136u16)
+        Init(16u16, as_init),
+        Error(17u16, as_error),
+        Ping(18u16, as_ping),
+        Pong(19u16, as_pong),
+        OpenChannel(32u16, as_open_channel),
+        AcceptChannel(33u16, as_accept_channel),
+        FundingCreated(34u16, as_funding_created),
+        FundingSigned(35u16, as_funding_signed),
+        FundingLocked(36u16, as_funding_locked),
+        ShutdownChannel(38u16, as_shutdown_channel),
+        ClosingNegotiation(39u16, as_closing_negotiation),
+        UpdateAddHtlc(128u16, as_update_add_htlc),
+        UpdateFulfillHtlc(130u16, as_update_fulfill_htlc),
+        UpdateFailHtlc(131u16, as_update_fail_htlc),
+        UpdateFailMalformedHtlc(135u16, as_update_fail_malformed_htlc),
+        CommitmentSigned(132u16, as_commitment_signed),
+        RevokeAndAck(133u16, as_revoke_and_ack),
+        UpdateFee(134u16, as_update_fee),
+        ReestablishChannel(136u16, as_reestablish_channel)
     }
 }
 
@@ -168,8 +178,22 @@ mod tests {
 
     #[test]
     fn open_channel_message(){
-        let msg_bytes = hex::decode("002006226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f3283054b8d351cfd58a790cb502069a64c40e226a0d228eae7e83e316dd2791700000000000186a00000000000000000000000000000023d0000000005e69ec000000000000003e800000000000003e8000030d4009001e302d254a0bc14d7c990d4c40e806bcaffc022ead28ba88eaa5450ef90565119020102859c2c7c7c0495198371dc0cb1329fdeca223972aeb089af2895c33b180cc6a20265ae921bd8cd25b7c62eda488b0f87955b3df36ccdb72cb0c75336cc8d5dc7030363b7cea6090e2f78a67a29a7cc5b351695a0dc6c0f2bbf14dc9098ed6074a3230213f314dcc6dbdaea4fac352277f55d53f873901477d80b8d2da794b411e5102202e19840efe9d300361f2624dfb5516f254bc6381be106c85ba0d3c429a54166c201").unwrap();
+        let msg_bytes = hex::decode(
+            "002006226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188\
+            910f3283054b8d351cfd58a790cb502069a64c40e226a0d228eae7e83e316dd2\
+            791700000000000186a00000000000000000000000000000023d0000000005e6\
+            9ec000000000000003e800000000000003e8000030d4009001e302d254a0bc14\
+            d7c990d4c40e806bcaffc022ead28ba88eaa5450ef90565119020102859c2c7c\
+            7c0495198371dc0cb1329fdeca223972aeb089af2895c33b180cc6a20265ae92\
+            1bd8cd25b7c62eda488b0f87955b3df36ccdb72cb0c75336cc8d5dc7030363b7\
+            cea6090e2f78a67a29a7cc5b351695a0dc6c0f2bbf14dc9098ed6074a3230213\
+            f314dcc6dbdaea4fac352277f55d53f873901477d80b8d2da794b411e5102202\
+            e19840efe9d300361f2624dfb5516f254bc6381be106c85ba0d3c429a54166c2\
+            01"
+        ).unwrap();
         let restored: Result<Message, _> = BinarySD::deserialize(msg_bytes.as_slice());
-        assert!(restored.is_ok());
+        let message = restored.unwrap();
+        let open_channel = message.as_open_channel();
+        assert!(open_channel.is_some());
     }
 }
