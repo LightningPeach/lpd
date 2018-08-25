163c54d6e0250f71a09d250d3187dfc0cc9a430b
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-08-25 20:34:19 +0300

    completed ChannelGraph trait declaration

--- routing/src/topology/node.rs
+++ routing/src/topology/node.rs
@@ -1,7 +1,9 @@
 use bitcoin_types::ChannelPoint;
 use common_types::Hash256;
 
-use secp256k1::PublicKey;
+use chrono::DateTime;
+use chrono::Utc;
+
 use std::net::SocketAddr;
 
 use wire::Color;
@@ -11,9 +13,11 @@ use wire::AnnouncementNodeData;
 use wire::AnnouncementNode;
 use wire::ShortChannelId;
 use wire::PublicKey;
-use wire::RawFeatureVector;
 use wire::Satoshi;
 use wire::Signature;
+use wire::Signed;
+use wire::ChannelUpdateFlags;
+use wire::MilliSatoshi;
 
 pub struct LightningNode {
     public_key: PublicKey,
@@ -42,6 +46,22 @@ pub struct ChannelInfo {
     auth_proof: ChannelAuthProof,
     channel_point: ChannelPoint,
     capacity: Satoshi,
+    // TODO(vlad):
+    db: (),
+}
+
+pub type ChannelPolicy = Signed<ChannelPolicyData>;
+
+// TODO: the struct is similar to wire::UpdateChannelData, reuse it somehow
+pub struct ChannelPolicyData {
+    short_channel_id: ShortChannelId,
+    last_update_time: DateTime<Utc>,
+    flags: ChannelUpdateFlags,
+    time_lock_delta: u16,
+    htlc_minimum: MilliSatoshi,
+    base_fee: u32,
+    fee_rate: u32,
+    node: LightningNode,
     // TODO(vlad):
     db: (),
 }
--- wire/src/message/channel/update.rs
+++ wire/src/message/channel/update.rs
@@ -5,13 +5,20 @@ use super::MilliSatoshi;
 
 pub type UpdateChannel = Signed<UpdateChannelData>;
 
+bitflags! {
+    #[derive(Serialize, Deserialize)]
+    pub struct ChannelUpdateFlags: u16 {
+        const DIRECTION = 0b00000001;
+        const DISABLED = 0b00000010;
+    }
+}
+
 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
 pub struct UpdateChannelData {
     hash: Hash256,
     short_channel_id: ShortChannelId,
     timestamp: u32,
-    // TODO: bitflags
-    flags: u16,
+    flags: ChannelUpdateFlags,
     time_lock_delta: u16,
     htlc_minimum: MilliSatoshi,
     base_fee: u32,
