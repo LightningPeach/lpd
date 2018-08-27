852d231de5aa4e97cbf432929939c91fdad97411
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-08-27 17:21:47 +0300

    added missing constructors

--- wire/src/message/channel/query_channel_range.rs
+++ wire/src/message/channel/query_channel_range.rs
@@ -8,6 +8,16 @@ pub struct QueryChannelRange {
     number_of_blocks: u32,
 }
 
+impl QueryChannelRange {
+    pub fn new(chain_hash: Hash256, first_block_height: u32, number_of_blocks: u32) -> Self {
+        QueryChannelRange {
+            chain_hash: chain_hash,
+            first_block_height: first_block_height,
+            number_of_blocks: number_of_blocks,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
 pub struct ReplyChannelRange {
     chain_hash: Hash256,
