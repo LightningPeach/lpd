f42b4430ab760149a1e324ac55483c9c4ca1fcfb
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-09-02 21:05:44 +0300

    fixed address obtaining, added LnRunning in order to exploit raii

--- testenv/src/lib.rs
+++ testenv/src/lib.rs
@@ -1,4 +1,5 @@
 #![forbid(unsafe_code)]
+#![allow(non_shorthand_field_patterns)]
 
 extern crate wire;
 extern crate brontide;
@@ -14,11 +15,14 @@ pub use self::btc::BtcDaemon;
 
 mod ln;
 pub use self::ln::LnDaemon;
+pub use self::ln::LnRunning;
 
 #[cfg(test)]
 mod tests {
     use super::BtcDaemon;
     use super::LnDaemon;
+    use super::LnRunning;
+
     use lnd_rust::rpc_grpc::LightningClient;
     use lnd_rust::rpc_grpc::Lightning;
     use lnd_rust::rpc;
@@ -36,15 +40,18 @@ mod tests {
             .unwrap()
             .with(|btcd| {
                 // creating two nodes with base port 10000
-                LnDaemon::with(2, 10000, btcd, |nodes| {
+                LnRunning::with(2, 10000, btcd, |nodes| {
+                    thread::sleep(Duration::from_secs(10));
                     let client = nodes[0].client().unwrap();
-                    let peer_address = nodes[1].address().unwrap();
+                    let peer_address = nodes[1].address()
+                        .unwrap().wait().unwrap();
                     let mut connect_peer_request = rpc::ConnectPeerRequest::new();
                     connect_peer_request.set_addr(peer_address);
                     let options = RequestOptions::new();
                     // try to connect first peer to second
                     let response = Lightning::connect_peer(&client, options, connect_peer_request);
                     let v = response.0.wait().unwrap().1.wait().unwrap().0;
+                    println!("{:?}", v);
                     thread::sleep(Duration::from_secs(100))
                 })
             })
