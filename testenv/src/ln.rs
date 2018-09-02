cceb3264b2406d6b40aa49d45fe1166af0118d97
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-09-02 21:05:44 +0300

    fixed address obtaining, added LnRunning in order to exploit raii

--- testenv/src/ln.rs
+++ testenv/src/ln.rs
@@ -9,12 +9,11 @@ use std::str::FromStr;
 use std::process::Command;
 use std::process::Child;
 use std::io;
-use std::fs::File;
 
 use lnd_rust::rpc::LightningAddress;
 use lnd_rust::rpc_grpc::LightningClient;
-use lnd_rust::tls_certificate::TLSCertificate;
 use grpc;
+use futures::Future;
 
 pub struct LnDaemon<'a> {
     peer_port: u16,
@@ -22,7 +21,11 @@ pub struct LnDaemon<'a> {
     rest_port: u16,
     home: Home,
     btcd: &'a BtcDaemon,
-    instance: Option<Child>,
+}
+
+pub struct LnRunning<'a> {
+    daemon: LnDaemon<'a>,
+    instance: Child,
 }
 
 impl<'a> LnDaemon<'a> {
@@ -39,14 +42,11 @@ impl<'a> LnDaemon<'a> {
             rest_port: rest_port,
             home: Home::new(name)?,
             btcd: btcd,
-            instance: None,
         })
     }
 
-    pub fn run(&mut self) -> Result<(), io::Error> {
-        self.terminate()?;
-
-        self.instance = Some(Command::new("lnd")
+    pub fn run(self) -> Result<LnRunning<'a>, io::Error> {
+        Command::new("lnd")
             .args(&[
                 "--bitcoin.active", "--bitcoin.simnet", "--noencryptwallet", "--no-macaroons",
                 "--btcd.rpcuser=devuser", "--btcd.rpcpass=devpass"
@@ -63,65 +63,74 @@ impl<'a> LnDaemon<'a> {
                 format!("--rpclisten=localhost:{}", self.rpc_port),
                 format!("--restlisten=localhost:{}", self.rest_port),
             ])
-            .spawn()?
-        );
-
-        Ok(())
+            .spawn()
+            .map(|instance| LnRunning {
+                daemon: self,
+                instance: instance,
+            })
     }
+}
 
-    pub fn terminate(&mut self) -> Result<(), io::Error> {
-        if let Some(ref mut instance) = self.instance {
-            instance.kill()?;
-        }
-
-        self.instance = None;
-
-        Ok(())
-    }
-
+impl<'a> LnRunning<'a> {
     // errors ignored
     pub fn with<F, T>(limit: u16, base_port: u16, btcd: &'a BtcDaemon, op: F) -> T where
         F: FnOnce(&mut Vec<Self>) -> T
     {
         let mut nodes = (0..limit).into_iter()
-            .map(|index| -> Result<LnDaemon, io::Error> {
+            .map(|index| -> Result<LnRunning, io::Error> {
                 let p_peer = base_port + index * 10;
                 let p_rpc = base_port + index * 10 + 1;
                 let p_rest = base_port + index * 10 + 2;
                 let name = format!("lnd-node-{}", index);
-                let mut lnd = LnDaemon::new(
+                LnDaemon::new(
                     p_peer, p_rpc, p_rest, name.as_str(), &btcd
-                )?;
-                lnd.run()?;
-                Ok(lnd)
+                )?.run()
             })
             .filter_map(Result::ok)
             .collect::<Vec<_>>();
-        let value = op(&mut nodes);
-        nodes.iter_mut().for_each(|node| node.terminate().unwrap_or(()));
-        value
+        op(&mut nodes)
     }
 
     pub fn client(&self) -> Result<LightningClient, io::Error> {
-        let certificate = TLSCertificate::from_der_path(self.home.public_key_path())?;
+        use lnd_rust::tls_certificate::TLSCertificate;
+
+        let &LnRunning {
+            daemon: ref daemon,
+            instance: _,
+        } = self;
+        let certificate = TLSCertificate::from_der_path(daemon.home.public_key_path())?;
         let localhost = "127.0.0.1";
         let conf = Default::default();
         let tls = certificate.into_tls(localhost)?;
-        let socket_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::from_str(localhost).unwrap()), self.rpc_port);
+        let socket_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::from_str(localhost).unwrap()), daemon.rpc_port);
         let c = grpc::Client::new_expl(&socket_address, localhost, tls, conf)?;
         Ok(LightningClient::with_client(c))
     }
 
-    pub fn address(&self) -> Result<LightningAddress, io::Error> {
+    pub fn address(&self) -> Result<impl Future<Item=LightningAddress, Error=grpc::Error>, io::Error> {
+        use lnd_rust::rpc;
+        use lnd_rust::rpc_grpc::Lightning;
+        use grpc::RequestOptions;
+
         let mut address = LightningAddress::new();
-        address.set_host(format!("127.0.0.1:{}", self.peer_port));
-        let public_key = {
-            let mut public_key_file = File::open(self.home.public_key_path())?;
-            let mut public_key = String::new();
-            io::Read::read_to_string(&mut public_key_file, &mut public_key)?;
-            public_key
-        };
-        address.set_pubkey(public_key);
-        Ok(address)
+        address.set_host(format!("127.0.0.1:{}", self.daemon.peer_port));
+
+        self
+            .client()
+            .map(move |client| {
+                client
+                    .get_info(RequestOptions::new(), rpc::GetInfoRequest::new())
+                    .drop_metadata()
+                    .map(move |response| {
+                        address.set_pubkey(response.identity_pubkey);
+                        address
+                    })
+            })
+    }
+}
+
+impl<'a> Drop for LnRunning<'a> {
+    fn drop(&mut self) {
+        self.instance.kill().unwrap()
     }
 }
