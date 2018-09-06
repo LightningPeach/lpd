b9dd014dfbae3a49a47a971ab400d31180615bb0
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-09-06 13:26:12 +0300

    added dummy open channel

--- testenv/src/ln.rs
+++ testenv/src/ln.rs
@@ -1,19 +1,22 @@
 use super::Home;
 use super::BtcDaemon;
 
-use std::net::SocketAddr;
-use std::net::Ipv4Addr;
-use std::net::IpAddr;
-use std::str::FromStr;
-
 use std::process::Command;
 use std::process::Child;
 use std::io;
 
+use lnd_rust::rpc::GetInfoResponse;
 use lnd_rust::rpc::LightningAddress;
+use lnd_rust::rpc::ConnectPeerResponse;
+use lnd_rust::rpc::OpenStatusUpdate;
+
 use lnd_rust::rpc_grpc::LightningClient;
 use grpc;
+
 use futures::Future;
+use futures::Stream;
+
+use lazycell::LazyCell;
 
 pub struct LnDaemon<'a> {
     peer_port: u16,
@@ -26,7 +29,8 @@ pub struct LnDaemon<'a> {
 pub struct LnRunning<'a> {
     daemon: LnDaemon<'a>,
     instance: Child,
-    client: LightningClient,
+    client: LazyCell<LightningClient>,
+    info: LazyCell<GetInfoResponse>,
 }
 
 impl<'a> LnDaemon<'a> {
@@ -65,21 +69,13 @@ impl<'a> LnDaemon<'a> {
                 format!("--restlisten=localhost:{}", self.rest_port),
             ])
             .spawn()
-            .and_then(|instance| {
-                use lnd_rust::tls_certificate::TLSCertificate;
-
-                let certificate = TLSCertificate::from_path(self.home.public_key_path())?;
-                let localhost = "127.0.0.1";
-                let tls = certificate.into_tls(localhost)?;
-                let socket_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::from_str(localhost).unwrap()), self.rpc_port);
-                let conf = Default::default();
-                let c = grpc::Client::new_expl(&socket_address, localhost, tls, conf)?;
-
-                Ok(LnRunning {
+            .map(|instance| {
+                LnRunning {
                     daemon: self,
                     instance: instance,
-                    client: LightningClient::with_client(c),
-                })
+                    client: LazyCell::new(),
+                    info: LazyCell::new(),
+                }
             })
     }
 }
@@ -101,25 +97,89 @@ impl<'a> LnRunning<'a> {
             .collect()
     }
 
-    pub fn client(&self) -> &LightningClient {
-        &self.client
+    fn new_client(&self) -> Result<LightningClient, grpc::Error> {
+        use std::net::SocketAddr;
+        use std::net::Ipv4Addr;
+        use std::net::IpAddr;
+        use std::str::FromStr;
+        use lnd_rust::tls_certificate::TLSCertificate;
+
+        let daemon = &self.daemon;
+
+        let certificate = TLSCertificate::from_path(daemon.home.public_key_path())
+            .map_err(grpc::Error::Io)?;
+        let localhost = "127.0.0.1";
+        let localhost_ip = IpAddr::V4(Ipv4Addr::from_str(localhost).unwrap());
+        let tls = certificate.into_tls(localhost)
+            .map_err(|e| grpc::Error::Io(e.into()))?;
+        let socket_address = SocketAddr::new(localhost_ip, daemon.rpc_port);
+        let conf = Default::default();
+        let inner = grpc::Client::new_expl(&socket_address, localhost, tls, conf)?;
+        Ok(LightningClient::with_client(inner))
     }
 
-    pub fn address(&self) -> impl Future<Item=LightningAddress, Error=grpc::Error> {
+    fn obtain_info(&self) -> impl Future<Item=GetInfoResponse, Error=grpc::Error> {
+        use lnd_rust::rpc::GetInfoRequest;
+        use lnd_rust::rpc_grpc::Lightning;
+        use grpc::RequestOptions;
+
+        self.client()
+            .get_info(RequestOptions::new(), GetInfoRequest::new())
+            .drop_metadata()
+    }
+
+    /// might panic
+    pub fn client(&self) -> &LightningClient {
+        self.client.borrow().unwrap_or_else(|| {
+            self.client.fill(self.new_client().unwrap()).ok().unwrap();
+            self.client()
+        })
+    }
+
+    /// wait first time, might panic
+    pub fn info(&self) -> &GetInfoResponse {
+        self.info.borrow().unwrap_or_else(|| {
+            self.info.fill(self.obtain_info().wait().unwrap()).ok().unwrap();
+            self.info()
+        })
+    }
+
+    pub fn address(&self) -> LightningAddress {
+        let mut address = LightningAddress::new();
+        address.set_host(format!("127.0.0.1:{}", self.daemon.peer_port));
+        address.set_pubkey(self.info().get_identity_pubkey().to_owned());
+        address
+    }
+
+    pub fn connect_peer(&self, peer: &Self) -> impl Future<Item=ConnectPeerResponse, Error=grpc::Error> {
         use lnd_rust::rpc;
         use lnd_rust::rpc_grpc::Lightning;
         use grpc::RequestOptions;
 
-        let mut address = LightningAddress::new();
-        address.set_host(format!("127.0.0.1:{}", self.daemon.peer_port));
-
-        self.client
-            .get_info(RequestOptions::new(), rpc::GetInfoRequest::new())
+        let mut request = rpc::ConnectPeerRequest::new();
+        request.set_addr(peer.address());
+        self.client()
+            .connect_peer(RequestOptions::new(), request)
             .drop_metadata()
-            .map(move |response| {
-                address.set_pubkey(response.identity_pubkey);
-                address
-            })
+    }
+
+    pub fn open_channel(&self, peer: &Self) -> impl Stream<Item=OpenStatusUpdate, Error=grpc::Error> {
+        use lnd_rust::rpc;
+        use lnd_rust::rpc_grpc::Lightning;
+        use grpc::RequestOptions;
+
+        let peer_address = peer.address();
+        let mut request = rpc::OpenChannelRequest::new();
+        request.set_node_pubkey_string(peer_address.get_pubkey().to_owned());
+        request.set_local_funding_amount(100000);
+        request.set_min_htlc_msat(1000);
+        request.set_push_sat(0);
+        request.set_remote_csv_delay(144);
+        request.set_sat_per_byte(12500);
+        request.set_target_conf(0);
+        self.client()
+            .open_channel(RequestOptions::new(), request)
+            .drop_metadata()
     }
 }
 
