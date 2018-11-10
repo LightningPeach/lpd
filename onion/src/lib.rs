1d032e2da58c0ab2669106630072fb5406d1b112
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-11-10 16:16:37 +0200

    implemented generate_shared_secrets, not tested yet

--- onion/src/lib.rs
+++ onion/src/lib.rs
@@ -42,21 +42,54 @@ pub struct HopData {
     header_mac: [u8; 32],
 }
 
-fn generate_shared_secrets(payment_path: &[PublicKey], session_key: &SecretKey) -> Result<Vec<[u8; 32]>, EcdsaError> {
+/// generate shared secrets by the given `payment_path` and `session_key`
+pub fn generate_shared_secrets(
+    payment_path: &[PublicKey],
+    session_key: &SecretKey,
+) -> Result<Vec<Hash256>, EcdsaError> {
     use secp256k1::Secp256k1;
 
-    let number = payment_path.len();
-    let mut last_ephemeral_pub_key = PublicKey::from_secret_key(&Secp256k1::new(), session_key)?;
+    let context = Secp256k1::new();
 
-    unimplemented!()
-}
+    // functions
+    // `mul_pk` and `mul_sk` obviously performs the multiplication in the elliptic curve group
+    // `hash` or `hash_s` computes a sha256 hash from a given slice or slices
+    // `hash_to_sk` obviously casts a sha256 hash into a secret key
+    let mul_pk = |x: &PublicKey, sk: &SecretKey| {
+        let mut temp = x.clone();
+        temp.mul_assign(&context, sk).map(|()| temp)
+    };
+    let mul_sk = |x: &SecretKey, sk: &SecretKey| {
+        let mut temp: SecretKey = x.clone();
+        temp.mul_assign(&context, sk).map(|()| temp)
+    };
+    let hash = |x: &[u8]| -> Hash256 { Hash256::from(x) };
+    let hash_s = |xs: &[&[u8]]| -> Hash256 { Hash256::from(xs) };
+    let hash_to_sk = |hash: &Hash256| SecretKey::from_slice(&context, hash.as_ref());
 
-// ecdh performs an ECDH operation between pk and sk. The returned value is
-// the sha256 of the compressed shared point.
-fn ecdh(pk: &PublicKey, sk: &SecretKey) -> Result<Hash256, EcdsaError> {
-    use secp256k1::Secp256k1;
+    // hardcoded public key
+    let base_point = {
+        let s = "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
+        PublicKey::from_slice(&context, hex::decode(s).unwrap().as_slice()).unwrap()
+    };
 
-    let mut pk_cloned = pk.clone();
-    pk_cloned.mul_assign(&Secp256k1::new(), sk)?;
-    Ok(Hash256::from(&pk_cloned.serialize()[..]))
+    let context = (
+        Vec::with_capacity(payment_path.len()),
+        session_key.clone(),
+        PublicKey::from_secret_key(&context, session_key)?,
+        Hash256::from([0; 32]),
+    );
+
+    payment_path
+        .iter()
+        .try_fold(context, |(mut v, secret, public, blinding), path_point| {
+            let temp = mul_pk(&path_point, &secret)?;
+            let result = hash(&temp.serialize()[..]);
+            let secret = mul_sk(&secret, &hash_to_sk(&blinding)?)?;
+            let blinding = hash_s(&[&public.serialize()[..], result.as_ref()][..]);
+            let public = mul_pk(&base_point, &secret)?;
+
+            v.push(result);
+            Ok((v, secret, public, blinding))
+        }).map(|(v, _, _, _)| v)
 }
