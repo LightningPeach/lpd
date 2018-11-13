1d58a1b4dc4b14977997a9755f9a3b010abe65b2
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-11-13 16:00:48 +0200

    added smallvec optimization, added helpers structures

--- onion/src/lib.rs
+++ onion/src/lib.rs
@@ -9,20 +9,12 @@ extern crate chacha;
 extern crate sha2;
 extern crate serde;
 extern crate serde_derive;
+extern crate smallvec;
 
 mod hop;
 mod route;
 
 pub use self::route::{OnionPacketVersion, OnionRoute, OnionPacket};
-pub use self::hop::{Hop, HopData, HopDataRealm};
+pub use self::hop::{Hop, HopBytes, HopData, HopDataRealm};
 
 use serde_derive::{Serialize, Deserialize};
-
-/// `HMAC_SIZE` is the length of the HMACs used to verify the integrity of
-/// the onion. Any value lower than 32 will truncate the HMAC both
-/// during onion creation as well as during the verification.
-pub const HMAC_SIZE: usize = 32;
-
-/// `NUM_MAX_HOPS` is the the maximum path length. This should be set to an
-/// estimate of the upper limit of the diameter of the node graph.
-pub const NUM_MAX_HOPS: usize = 20;
