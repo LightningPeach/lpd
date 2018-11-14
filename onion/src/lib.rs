7f7fea5907b332fc68f25d7b17de1af56adc5309
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-11-14 16:57:00 +0200

    implemented packed generation, not tested yet

--- onion/src/lib.rs
+++ onion/src/lib.rs
@@ -9,12 +9,9 @@ extern crate chacha;
 extern crate sha2;
 extern crate serde;
 extern crate serde_derive;
-extern crate smallvec;
 
 mod hop;
 mod route;
 
 pub use self::route::{OnionPacketVersion, OnionRoute, OnionPacket};
 pub use self::hop::{Hop, HopBytes, HopData, HopDataRealm};
-
-use serde_derive::{Serialize, Deserialize};
