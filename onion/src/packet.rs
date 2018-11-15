d602449ecceb92b3f65a1a3da3864d67f66fe3ce
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-11-15 15:04:52 +0200

    added new error type

--- onion/src/packet.rs
+++ onion/src/packet.rs
@@ -12,17 +12,6 @@ pub struct OnionPacket {
     pub(crate) hmac: HmacData,
 }
 
-#[derive(Debug, Eq, PartialEq, Serialize)]
-pub struct ValidOnionPacket(pub(crate) OnionPacket);
-
-pub enum Processed {
-    ExitNode,
-    MoreHops {
-        next: OnionPacket,
-        forwarding_instructions: Hop,
-    },
-}
-
 impl OnionPacket {
     /// `NUM_MAX_HOPS` is the the maximum path length. This should be set to an
     /// estimate of the upper limit of the diameter of the node graph.
@@ -44,15 +33,32 @@ impl OnionPacket {
     }
 }
 
+#[derive(Debug, Eq, PartialEq, Serialize)]
+pub struct ValidOnionPacket(pub(crate) OnionPacket);
+
+pub enum OnionPacketProcessingError {
+    EcdsaError(EcdsaError),
+    WrongHmac,
+}
+
+pub enum Processed {
+    ExitNode,
+    MoreHops {
+        next: OnionPacket,
+        forwarding_instructions: Hop,
+    },
+}
+
 impl ValidOnionPacket {
     pub fn process(
         self,
         associated_data: Vec<u8>,
         incoming_cltv: u32,
         onion_key: &SecretKey,
-    ) -> Result<Processed, EcdsaError> {
+    ) -> Result<Processed, OnionPacketProcessingError> {
         use secp256k1::Secp256k1;
         use wire::BinarySD;
+        use self::OnionPacketProcessingError::*;
 
         let context = Secp256k1::new();
 
@@ -66,7 +72,7 @@ impl ValidOnionPacket {
         };
         let hash = |x: &[u8]| -> Hash256 { Hash256::from(x) };
 
-        let temp = mul_pk(ephemeral_key.as_ref(), onion_key)?;
+        let temp = mul_pk(ephemeral_key.as_ref(), onion_key).map_err(EcdsaError)?;
         let shared_secret = hash(&temp.serialize()[..]);
 
         let mut data = Vec::with_capacity(HopBytes::SIZE * OnionPacket::NUM_MAX_HOPS);
@@ -77,9 +83,12 @@ impl ValidOnionPacket {
             shared_secret,
             &[data.as_slice(), associated_data.as_slice()],
         );
-        assert_eq!(hmac, hmac_calc);
 
-        let _ = incoming_cltv;
-        unimplemented!()
+        if hmac_calc != hmac {
+            Err(WrongHmac)
+        } else {
+            let _ = incoming_cltv;
+            unimplemented!()
+        }
     }
 }
