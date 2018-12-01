319e18689444d334bb6c73003ae1d569ee829649
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-12-01 23:58:18 +0200

    [LNP-986] Disable regenerating code if up to date

--- rpc/interface/build.rs
+++ rpc/interface/build.rs
@@ -3,10 +3,33 @@ extern crate protoc_rust_grpc;
 use protoc_rust_grpc::{Args, Error};
 
 fn main() -> Result<(), Error> {
-    protoc_rust_grpc::run(Args {
-        out_dir: "src",
-        includes: &["."],
-        input: &["common.proto", "payment.proto", "routing.proto", "channel.proto"],
-        rust_protobuf: true,
-    })
+    use std::fs;
+
+    let inputs = vec!["common", "payment", "routing", "channel"];
+    let inputs = inputs.into_iter().filter(|&name| {
+        let input = format!("{}.proto", name);
+        let output = format!("src/{}.rs", name);
+
+        match fs::metadata(output) {
+            Ok(metadata) => match metadata.modified() {
+                Ok(output_time) => {
+                    let input_time = fs::metadata(input).unwrap().modified().unwrap();
+                    input_time > output_time
+                },
+                Err(_) => true,
+            },
+            Err(_) => true,
+        }
+    }).collect::<Vec<_>>();
+
+    if !inputs.is_empty() {
+        protoc_rust_grpc::run(Args {
+            out_dir: "src",
+            includes: &["."],
+            input: inputs.as_slice(),
+            rust_protobuf: true,
+        })
+    } else {
+        Ok(())
+    }
 }
