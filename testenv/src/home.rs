d86995b5a27b2bad2cdd032938c4785301cc73c6
Mykola Sakhno <mykola.sakhno@bitfury.com>
2019-02-18 23:23:34 +0200

    fix issues with new lnd
    Use --noseedbackup. Specify application dir for lnd and specify empty config file.

--- testenv/src/home.rs
+++ testenv/src/home.rs
@@ -39,6 +39,10 @@ impl Home {
         self.ext_path("rpc.key")
     }
 
+    pub fn lnd_conf_path(&self) -> PathBuf {
+        self.ext_path("lnd.conf")
+    }
+
     pub fn new(name: &str, force: bool) -> Result<Self, io::Error> {
         let s = Home {
             name: name.to_owned(),
@@ -59,6 +63,13 @@ impl Home {
                 })?;
         }
         fs::File::create(lock_path)?;
+
+        // lnd tries to open default config file if it is unspecified in its options
+        // so we create empty one
+        use std::io::Write;
+        let lnd_config_path = s.lnd_conf_path();
+        let mut lnd_conf_file = fs::File::create(lnd_config_path)?;
+        lnd_conf_file.write_all(b"[Application Options]\n")?;
 
         let _ = Command::new("gencerts")
             .current_dir(s.path()).output()?;
