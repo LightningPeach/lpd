98281da0c87a58732046d0ac10bf0d9d53939245
Vladislav Melnik <vladislav.melnik@bitfury.com>
2019-04-25 14:08:20 +0300

    remove wrong and redundant check; the check is wrong because the size of this element compares with the size of next element which ok to be different; the check is redundant because `SeqAccess::next_element` do such check

--- binformat/src/compression_facade.rs
+++ binformat/src/compression_facade.rs
@@ -61,15 +61,11 @@ impl<'de, T> Deserialize<'de> for SerdeVec<T> where T: PackSized + de::Deseriali
                     if size == 0 { break; }
                     let element: T = seq.next_element()?
                         .ok_or(de::Error::custom(format!("cannot read T")))?;
-                    if size < element.pack_size() {
-                        return Err(de::Error::custom(format!("cannot assemble integer amount of T")))
-                    }
                     size -= element.pack_size();
                     data.push(element);
                 }
                 Ok(SerdeVec(data))
             }
-
         }
 
         deserializer.deserialize_seq(Visitor {
