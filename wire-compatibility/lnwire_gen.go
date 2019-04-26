9bae0435f32b37863555a7a082b9f02aac010ac5
Vladislav Melnik <vladislav.melnik@bitfury.com>
2019-04-26 18:31:30 +0300

    improve testing tool, test compatibility in both directions

--- wire-compatibility/lnwire_gen.go
+++ wire-compatibility/lnwire_gen.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/binary"
+	"image/color"
 	"io"
+	"net"
 	"os"
 
+	"github.com/btcsuite/btcutil"
+
 	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/lightningnetwork/lnd/lnwire"
+
+	"github.com/btcsuite/btcd/wire"
 )
 
 func randPubKey() (*btcec.PublicKey, error) {
@@ -35,10 +43,11 @@ func writeMessage(w io.Writer, msg lnwire.Message) {
 	}
 }
 
-
 func createRevokeAndAck() lnwire.Message {
 	var chanId lnwire.ChannelID
 	var revocation [32]byte
+	chanId[0] = 1
+	revocation[1] = 2
 	privkey, _ := btcec.NewPrivateKey(btcec.S256())
 	pubkey := privkey.PubKey()
 	msg := &lnwire.RevokeAndAck{
@@ -49,6 +58,645 @@ func createRevokeAndAck() lnwire.Message {
 	return msg
 }
 
+func createFundingLocked() lnwire.Message {
+	var chanId lnwire.ChannelID
+	chanId[0] = 1
+	privkey, _ := btcec.NewPrivateKey(btcec.S256())
+	pubkey := privkey.PubKey()
+	msg := &lnwire.FundingLocked{
+		ChanID:                 chanId,
+		NextPerCommitmentPoint: pubkey,
+	}
+	return msg
+}
+
+func createAnnounceSignatures() lnwire.Message {
+	var chanId lnwire.ChannelID
+	chanId[0] = 1
+	shortChannelID := lnwire.ShortChannelID{
+		BlockHeight: 100,
+		TxIndex:     10,
+		TxPosition:  1,
+	}
+	var nodeSignature lnwire.Sig
+	nodeSignature[2] = 3
+
+	var bitcoinSignature lnwire.Sig
+	bitcoinSignature[1] = 4
+
+	extraOpaqueData := []byte{1, 2, 3, 100}
+
+	msg := &lnwire.AnnounceSignatures{
+		ChannelID:        chanId,
+		ShortChannelID:   shortChannelID,
+		NodeSignature:    nodeSignature,
+		BitcoinSignature: bitcoinSignature,
+		ExtraOpaqueData:  extraOpaqueData,
+	}
+	return msg
+}
+
+func as33Byte(a []byte) [33]byte {
+	if len(a) != 33 {
+		panic("incorrect length")
+	}
+	var b [33]byte
+	copy(b[:], a)
+
+	return b
+}
+
+func createChannelAnnouncement() lnwire.Message {
+	var nodeSig1 lnwire.Sig
+	nodeSig1[2] = 3
+
+	var nodeSig2 lnwire.Sig
+	nodeSig2[3] = 4
+
+	var bitcoinSig1 lnwire.Sig
+	bitcoinSig1[1] = 2
+
+	var bitcoinSig2 lnwire.Sig
+	bitcoinSig2[2] = 5
+
+	features := lnwire.NewRawFeatureVector(lnwire.DataLossProtectOptional, lnwire.GossipQueriesOptional)
+
+	var chainHash chainhash.Hash
+	chainHash[1] = 11
+
+	shortChannelId := lnwire.ShortChannelID{
+		TxPosition:  100,
+		TxIndex:     2,
+		BlockHeight: 1234,
+	}
+
+	nodeId1, _ := randPubKey()
+	nodeId2, _ := randPubKey()
+
+	bitcoinKey1, _ := randPubKey()
+
+	bitcoinKey2, _ := randPubKey()
+
+	// TODO(mkl): do not exist in spec, but do exist in lnd
+	extraOpaqueData := []byte{}
+
+	msg := &lnwire.ChannelAnnouncement{
+		NodeSig1: nodeSig1,
+		NodeSig2: nodeSig2,
+
+		BitcoinSig1: bitcoinSig1,
+		BitcoinSig2: bitcoinSig2,
+
+		Features: features,
+
+		ChainHash: chainHash,
+
+		ShortChannelID: shortChannelId,
+
+		NodeID1: as33Byte(nodeId1.SerializeCompressed()),
+		NodeID2: as33Byte(nodeId2.SerializeCompressed()),
+
+		BitcoinKey1: as33Byte(bitcoinKey1.SerializeCompressed()),
+		BitcoinKey2: as33Byte(bitcoinKey2.SerializeCompressed()),
+
+		ExtraOpaqueData: extraOpaqueData,
+	}
+
+	return msg
+}
+
+func createChannelReestablish() lnwire.Message {
+	var chanId lnwire.ChannelID
+	chanId[0] = 1
+
+	nextLocalCommitHeight := uint64(11)
+
+	remoteCommitTailHeight := uint64(2)
+
+	var lastRemoteCommitSecret [32]byte
+	lastRemoteCommitSecret[1] = 2
+
+	localUnrevokedCommitPoint, _ := randPubKey()
+
+	msg := &lnwire.ChannelReestablish{
+		ChanID:                    chanId,
+		NextLocalCommitHeight:     nextLocalCommitHeight,
+		RemoteCommitTailHeight:    remoteCommitTailHeight,
+		LastRemoteCommitSecret:    lastRemoteCommitSecret,
+		LocalUnrevokedCommitPoint: localUnrevokedCommitPoint,
+	}
+	return msg
+}
+
+func createChannelUpdate() lnwire.Message {
+	var signature lnwire.Sig
+	signature[2] = 3
+
+	var chainHash chainhash.Hash
+	chainHash[1] = 4
+
+	shortChannelId := lnwire.ShortChannelID{
+		BlockHeight: 100,
+		TxIndex:     4,
+		TxPosition:  15,
+	}
+
+	timestamp := uint32(1000000)
+
+	messageFlags := lnwire.ChanUpdateOptionMaxHtlc
+
+	channelFlags := lnwire.ChanUpdateDirection
+
+	timeLockDelta := uint16(100)
+
+	htlcMinimumMsat := lnwire.MilliSatoshi(1000)
+
+	baseFee := uint32(100)
+
+	feeRate := uint32(5)
+
+	htlcMaximumMsat := lnwire.MilliSatoshi(100000000)
+
+	extraOpaqueData := []byte{}
+
+	msg := &lnwire.ChannelUpdate{
+		Signature:       signature,
+		ChainHash:       chainHash,
+		ShortChannelID:  shortChannelId,
+		Timestamp:       timestamp,
+		MessageFlags:    messageFlags,
+		ChannelFlags:    channelFlags,
+		TimeLockDelta:   timeLockDelta,
+		HtlcMinimumMsat: htlcMinimumMsat,
+		BaseFee:         baseFee,
+		FeeRate:         feeRate,
+		HtlcMaximumMsat: htlcMaximumMsat,
+		ExtraOpaqueData: extraOpaqueData,
+	}
+
+	return msg
+
+}
+
+func createClosingSigned() lnwire.Message {
+	var chanId lnwire.ChannelID
+	chanId[0] = 1
+
+	feeSatoshis := btcutil.Amount(123)
+
+	var signature lnwire.Sig
+	signature[1] = 2
+
+	msg := &lnwire.ClosingSigned{
+		ChannelID:   chanId,
+		FeeSatoshis: feeSatoshis,
+		Signature:   signature,
+	}
+
+	return msg
+
+}
+
+func createCommitSig() lnwire.Message {
+	var chanId lnwire.ChannelID
+	chanId[0] = 1
+
+	var commitSig lnwire.Sig
+	commitSig[1] = 2
+
+	var htlcSig1 lnwire.Sig
+	htlcSig1[1] = 3
+
+	var htlcSig2 lnwire.Sig
+	htlcSig2[3] = 4
+
+	htlcSigs := []lnwire.Sig{htlcSig1, htlcSig2}
+
+	msg := &lnwire.CommitSig{
+		ChanID:    chanId,
+		CommitSig: commitSig,
+		HtlcSigs:  htlcSigs,
+	}
+
+	return msg
+}
+
+func createFundingCreated() lnwire.Message {
+	var pendingChannelID [32]byte
+	pendingChannelID[0] = 2
+
+	var fundingPointHash chainhash.Hash
+	fundingPointHash[3] = 5
+
+	fundingPoint := wire.OutPoint{
+		Index: 2,
+		Hash:  fundingPointHash,
+	}
+
+	var commitSig lnwire.Sig
+	commitSig[1] = 5
+
+	msg := &lnwire.FundingCreated{
+		PendingChannelID: pendingChannelID,
+		FundingPoint:     fundingPoint,
+		CommitSig:        commitSig,
+	}
+
+	return msg
+}
+
+func createFundingSigned() lnwire.Message {
+	var chanId lnwire.ChannelID
+	chanId[1] = 5
+
+	var commitSig lnwire.Sig
+	commitSig[1] = 3
+
+	msg := &lnwire.FundingSigned{
+		ChanID:    chanId,
+		CommitSig: commitSig,
+	}
+	return msg
+}
+
+func createGossipTimestampRange() lnwire.Message {
+	var chainHash chainhash.Hash
+	chainHash[1] = 11
+
+	firstTimestamp := uint32(100000000)
+
+	timestampRange := uint32(1234)
+
+	msg := &lnwire.GossipTimestampRange{
+		ChainHash:      chainHash,
+		FirstTimestamp: firstTimestamp,
+		TimestampRange: timestampRange,
+	}
+
+	return msg
+}
+
+func createInit() lnwire.Message {
+	globalFeatures := lnwire.NewRawFeatureVector()
+
+	localFeatures := lnwire.NewRawFeatureVector(
+		lnwire.DataLossProtectRequired,
+		lnwire.DataLossProtectOptional,
+		lnwire.InitialRoutingSync,
+		lnwire.GossipQueriesRequired,
+		lnwire.GossipQueriesOptional,
+	)
+
+	msg := &lnwire.Init{
+		GlobalFeatures: globalFeatures,
+		LocalFeatures:  localFeatures,
+	}
+
+	return msg
+
+}
+
+func createNodeAnnouncement() lnwire.Message {
+	var signature lnwire.Sig
+	signature[3] = 1
+
+	features := lnwire.NewRawFeatureVector()
+
+	timestamp := uint32(123331122)
+
+	pubKey, _ := randPubKey()
+	nodeId := as33Byte(pubKey.SerializeCompressed())
+
+	rgbColor := color.RGBA{
+		A: 100,
+		B: 10,
+		G: 20,
+		R: 33,
+	}
+
+	var alias lnwire.NodeAlias
+	alias[0] = 1
+
+	addresses := []net.Addr{
+		&net.TCPAddr{
+			IP:   net.ParseIP("127.0.0.1"),
+			Port: 10000,
+			Zone: "",
+		},
+		&net.TCPAddr{
+			IP:   net.ParseIP("9.9.9.9"),
+			Port: 1234,
+			Zone: "",
+		},
+		&net.TCPAddr{
+			IP:   net.ParseIP("10.10.101.21"),
+			Port: 11111,
+			Zone: "",
+		},
+	}
+
+	extraOpaqueData := []byte{}
+
+	msg := &lnwire.NodeAnnouncement{
+		Signature:       signature,
+		Features:        features,
+		Timestamp:       timestamp,
+		NodeID:          nodeId,
+		RGBColor:        rgbColor,
+		Alias:           alias,
+		Addresses:       addresses,
+		ExtraOpaqueData: extraOpaqueData,
+	}
+
+	return msg
+}
+
+func createOpenChannel() lnwire.Message {
+	var chainHash chainhash.Hash
+	chainHash[2] = 12
+
+	var pendingChannelId [32]byte
+	pendingChannelId[0] = 2
+
+	fundingAmount := btcutil.Amount(100000)
+
+	pushAmount := lnwire.MilliSatoshi(12341)
+
+	dustLimit := btcutil.Amount(200)
+
+	maxValueInFlight := lnwire.MilliSatoshi(10000)
+
+	channelReserve := btcutil.Amount(1000)
+
+	htlcMinimum := lnwire.MilliSatoshi(1000)
+
+	feePerKiloWeight := uint32(10)
+
+	csvDelay := uint16(15)
+
+	maxAcceptedHTLCs := uint16(10)
+
+	fundingKey, _ := randPubKey()
+
+	revocationPoint, _ := randPubKey()
+
+	paymentPoint, _ := randPubKey()
+
+	delayedPaymentPoint, _ := randPubKey()
+
+	htlcPoint, _ := randPubKey()
+
+	firstCommitmentPoint, _ := randPubKey()
+
+	channelFlags := lnwire.FFAnnounceChannel
+
+	msg := &lnwire.OpenChannel{
+		ChainHash:            chainHash,
+		PendingChannelID:     pendingChannelId,
+		FundingAmount:        fundingAmount,
+		PushAmount:           pushAmount,
+		DustLimit:            dustLimit,
+		MaxValueInFlight:     maxValueInFlight,
+		ChannelReserve:       channelReserve,
+		HtlcMinimum:          htlcMinimum,
+		FeePerKiloWeight:     feePerKiloWeight,
+		CsvDelay:             csvDelay,
+		MaxAcceptedHTLCs:     maxAcceptedHTLCs,
+		FundingKey:           fundingKey,
+		RevocationPoint:      revocationPoint,
+		PaymentPoint:         paymentPoint,
+		DelayedPaymentPoint:  delayedPaymentPoint,
+		HtlcPoint:            htlcPoint,
+		FirstCommitmentPoint: firstCommitmentPoint,
+		ChannelFlags:         channelFlags,
+	}
+
+	return msg
+}
+
+func createPing() lnwire.Message {
+	numPongBytes := uint16(10)
+	paddingBytes := lnwire.PingPayload{1, 2, 3, 4}
+	msg := &lnwire.Ping{
+		NumPongBytes: numPongBytes,
+		PaddingBytes: paddingBytes,
+	}
+	return msg
+}
+
+func createPong() lnwire.Message {
+	pongBytes := lnwire.PongPayload{1, 200}
+	msg := &lnwire.Pong{
+		PongBytes: pongBytes,
+	}
+	return msg
+}
+
+func createQueryChannelRange() lnwire.Message {
+	var chainHash chainhash.Hash
+	chainHash[2] = 11
+
+	firstBlockHeight := uint32(10000)
+
+	numBlocks := uint32(12)
+
+	msg := &lnwire.QueryChannelRange{
+		ChainHash:        chainHash,
+		FirstBlockHeight: firstBlockHeight,
+		NumBlocks:        numBlocks,
+	}
+
+	return msg
+}
+
+func createQueryShortChanIDs(encodingType lnwire.ShortChanIDEncoding) lnwire.Message {
+	var chainHash chainhash.Hash
+	chainHash[1] = 6
+
+	shortChanIdS := []lnwire.ShortChannelID{
+		lnwire.ShortChannelID{
+			TxPosition:  10,
+			TxIndex:     1,
+			BlockHeight: 12221,
+		},
+		lnwire.ShortChannelID{
+			TxPosition:  101,
+			TxIndex:     12,
+			BlockHeight: 15000,
+		},
+		lnwire.ShortChannelID{
+			TxPosition:  11,
+			TxIndex:     5,
+			BlockHeight: 200000,
+		},
+	}
+	msg := &lnwire.QueryShortChanIDs{
+		ChainHash:    chainHash,
+		EncodingType: encodingType,
+		ShortChanIDs: shortChanIdS,
+	}
+
+	return msg
+}
+
+func createReplyChannelRange(encodingType lnwire.ShortChanIDEncoding) lnwire.Message {
+	queryChannelRange := (createQueryChannelRange()).(*lnwire.QueryChannelRange)
+
+	complete := uint8(1)
+
+	shortChanIdS := []lnwire.ShortChannelID{
+		lnwire.ShortChannelID{
+			TxPosition:  10,
+			TxIndex:     1,
+			BlockHeight: 12221,
+		},
+		lnwire.ShortChannelID{
+			TxPosition:  101,
+			TxIndex:     12,
+			BlockHeight: 15000,
+		},
+		lnwire.ShortChannelID{
+			TxPosition:  11,
+			TxIndex:     5,
+			BlockHeight: 200000,
+		},
+	}
+
+	msg := &lnwire.ReplyChannelRange{
+		QueryChannelRange: *queryChannelRange,
+		Complete:          complete,
+		EncodingType:      encodingType,
+		ShortChanIDs:      shortChanIdS,
+	}
+
+	return msg
+}
+
+func createReplyShortChanIDsEnd() lnwire.Message {
+	var chainHash chainhash.Hash
+	chainHash[1] = 7
+
+	complete := uint8(1)
+
+	msg := &lnwire.ReplyShortChanIDsEnd{
+		ChainHash: chainHash,
+		Complete:  complete,
+	}
+
+	return msg
+}
+
+func createShutdown() lnwire.Message {
+	var chanelId lnwire.ChannelID
+	chanelId[0] = 1
+
+	address := lnwire.DeliveryAddress{1, 2, 3, 4, 5}
+
+	msg := &lnwire.Shutdown{
+		ChannelID: chanelId,
+		Address:   address,
+	}
+
+	return msg
+}
+
+func createUpdateAddHTLC() lnwire.Message {
+	var chanID lnwire.ChannelID
+	chanID[0] = 2
+
+	iD := uint64(1001)
+
+	amount := lnwire.MilliSatoshi(101000)
+
+	var paymentHash [32]byte
+	paymentHash[1] = 121
+
+	expiry := uint32(100)
+
+	var onionBlob [lnwire.OnionPacketSize]byte
+	onionBlob[1] = 5
+
+	msg := &lnwire.UpdateAddHTLC{
+		ChanID:      chanID,
+		ID:          iD,
+		Amount:      amount,
+		PaymentHash: paymentHash,
+		Expiry:      expiry,
+		OnionBlob:   onionBlob,
+	}
+	return msg
+}
+
+func createUpdateFailHTLC() lnwire.Message {
+	var chanID lnwire.ChannelID
+	chanID[0] = 4
+
+	iD := uint64(10010)
+
+	reason := lnwire.OpaqueReason{1, 2, 1, 5}
+
+	msg := &lnwire.UpdateFailHTLC{
+		ChanID: chanID,
+		ID:     iD,
+		Reason: reason,
+	}
+
+	return msg
+}
+
+func createUpdateFailMalformedHTLC() lnwire.Message {
+	var chanID lnwire.ChannelID
+	chanID[0] = 2
+
+	iD := uint64(100)
+
+	var shaOnionBlob [sha256.Size]byte
+	shaOnionBlob[3] = 5
+
+	failureCode := lnwire.CodeInvalidOnionHmac
+
+	msg := &lnwire.UpdateFailMalformedHTLC{
+		ChanID:       chanID,
+		ID:           iD,
+		ShaOnionBlob: shaOnionBlob,
+		FailureCode:  failureCode,
+	}
+
+	return msg
+}
+
+func createUpdateFee() lnwire.Message {
+	var chanID lnwire.ChannelID
+	chanID[0] = 2
+
+	feePerKw := uint32(1001)
+
+	msg := &lnwire.UpdateFee{
+		ChanID:   chanID,
+		FeePerKw: feePerKw,
+	}
+
+	return msg
+}
+
+func createUpdateFulfillHtlc() lnwire.Message {
+	var chanID lnwire.ChannelID
+	chanID[0] = 2
+
+	iD := uint64(121)
+
+	var paymentPreimage [32]byte
+	paymentPreimage[1] = 100
+
+	msg := &lnwire.UpdateFulfillHTLC{
+		ChanID:          chanID,
+		ID:              iD,
+		PaymentPreimage: paymentPreimage,
+	}
+
+	return msg
+}
+
 func main() {
 	var f, err = os.Create("/tmp/messages")
 	if err != nil {
@@ -56,19 +704,13 @@ func main() {
 	}
 	defer f.Close()
 
+	// TODO(mkl): refactor into some object, like TestDataProducer
 	pubkey, err := randPubKey()
 	if err != nil {
 		panic(err)
 	}
 
-	writeMessage(f, &lnwire.OpenChannel{
-		FundingKey:           pubkey,
-		RevocationPoint:      pubkey,
-		PaymentPoint:         pubkey,
-		DelayedPaymentPoint:  pubkey,
-		HtlcPoint:            pubkey,
-		FirstCommitmentPoint: pubkey,
-	})
+	writeMessage(f, createOpenChannel())
 	writeMessage(f, &lnwire.AcceptChannel{
 		FundingKey:           pubkey,
 		RevocationPoint:      pubkey,
@@ -77,8 +719,52 @@ func main() {
 		HtlcPoint:            pubkey,
 		FirstCommitmentPoint: pubkey,
 	})
-	writeMessage(f, &lnwire.FundingCreated{})
-	writeMessage(f, &lnwire.FundingSigned{})
-	writeMessage(f, &lnwire.FundingLocked{})
+	writeMessage(f, createFundingCreated())
+	writeMessage(f, createFundingSigned())
 	writeMessage(f, createRevokeAndAck())
+	writeMessage(f, createFundingLocked())
+
+	writeMessage(f, createAnnounceSignatures())
+
+	writeMessage(f, createChannelAnnouncement())
+
+	writeMessage(f, createChannelReestablish())
+
+	writeMessage(f, createChannelUpdate())
+
+	writeMessage(f, createClosingSigned())
+
+	writeMessage(f, createCommitSig())
+
+	writeMessage(f, createGossipTimestampRange())
+
+	writeMessage(f, createInit())
+
+	writeMessage(f, createNodeAnnouncement())
+
+	writeMessage(f, createPing())
+
+	writeMessage(f, createPong())
+
+	writeMessage(f, createQueryChannelRange())
+
+	writeMessage(f, createQueryShortChanIDs(lnwire.EncodingSortedPlain))
+	writeMessage(f, createQueryShortChanIDs(lnwire.EncodingSortedZlib))
+
+	writeMessage(f, createReplyChannelRange(lnwire.EncodingSortedPlain))
+	writeMessage(f, createReplyChannelRange(lnwire.EncodingSortedZlib))
+
+	writeMessage(f, createReplyShortChanIDsEnd())
+
+	writeMessage(f, createShutdown())
+
+	writeMessage(f, createUpdateAddHTLC())
+
+	writeMessage(f, createUpdateFailHTLC())
+
+	writeMessage(f, createUpdateFailMalformedHTLC())
+
+	writeMessage(f, createUpdateFee())
+
+	writeMessage(f, createUpdateFulfillHtlc())
 }
--- wire-compatibility/src/main.rs
+++ wire-compatibility/src/main.rs
@@ -1,7 +1,58 @@
-fn main() {
+use wire::MessageExt;
+use binformat::WireError;
+
+fn de(data: Vec<u8>) -> Result<Vec<MessageExt>, WireError> {
     use binformat::BinarySD;
-    use wire::Message;
-    use std::{fs::File, io::{Cursor, Read, Seek, SeekFrom}};
+    use wire::{Message, MessageExt};
+    use std::io::{Cursor, Seek, SeekFrom};
+
+    let mut messages = Vec::new();
+
+    let mut cursor = Cursor::new(data);
+    loop {
+        if cursor.get_ref().len() == cursor.position() as _ {
+            break;
+        }
+        let length = BinarySD::deserialize::<u16, _>(&mut cursor)?;
+        let position = cursor.position() as u16;
+        let msg = BinarySD::deserialize::<Message, _>(&mut cursor)?;
+
+        let extra_length = position + length - (cursor.position() as u16);
+        let mut extra_data = Vec::new();
+        if extra_length > 0 {
+            extra_data.extend_from_slice(&cursor.get_ref()[(cursor.position() as usize)..((position + length) as usize)]);
+            cursor.seek(SeekFrom::Current(extra_length as i64)).unwrap();
+        }
+        let message_ext = MessageExt {
+            message: msg,
+            extra_data: extra_data,
+        };
+        messages.push(message_ext);
+    }
+
+    Ok(messages)
+}
+
+fn ser(messages: &Vec<MessageExt>) -> Result<Vec<u8>, WireError> {
+    use binformat::BinarySD;
+
+    messages.iter()
+        .try_fold(Vec::new(), |mut x, message_ext| {
+            let length_position = x.len();
+            BinarySD::serialize(&mut x, &0u16)?;
+            let position = x.len();
+            BinarySD::serialize(&mut x, &message_ext.message)?;
+            x.extend_from_slice(message_ext.extra_data.as_ref());
+            let length = (x.len() - position) as u16;
+            let mut y = Vec::with_capacity(2);
+            BinarySD::serialize(&mut y, &length)?;
+            x[length_position..(length_position + 2)].copy_from_slice(y.as_ref());
+            Ok(x)
+        })
+}
+
+fn main() {
+    use std::{fs::File, io::Read};
     use std::process::Command;
 
     let _ = Command::new("go")
@@ -15,20 +66,9 @@ fn main() {
         v
     };
 
-    let mut cursor = Cursor::new(data);
-    loop {
-        if cursor.get_ref().len() == cursor.position() as _ {
-            break;
-        }
-        let length = BinarySD::deserialize::<u16, _>(&mut cursor).unwrap();
-        let position = cursor.position() as u16;
-        let msg = BinarySD::deserialize::<Message, _>(&mut cursor).unwrap();
-        println!("{:?}", msg);
-
-        let extra_length = position + length - (cursor.position() as u16);
-        if extra_length > 0 {
-            println!("WARNING: extra length: {}", extra_length);
-        }
-        cursor.seek(SeekFrom::Start((position + length) as u64)).unwrap();
+    let messages = de(data).unwrap();
+    for message in &messages {
+        println!("{:?}", message);
     }
+    assert_eq!(de(ser(&messages).unwrap()).unwrap(), messages);
 }
