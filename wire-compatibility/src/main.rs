3737fb87df6d1ab26967b367bcf3664a65d6eba5
Vladislav Melnik <vladislav.melnik@bitfury.com>
2019-05-03 15:20:37 +0300

    improve test tool

--- wire-compatibility/src/main.rs
+++ wire-compatibility/src/main.rs
@@ -52,23 +52,59 @@ fn ser(messages: &Vec<MessageExt>) -> Result<Vec<u8>, WireError> {
 }
 
 fn main() {
-    use std::{fs::File, io::Read};
-    use std::process::Command;
+    use clap::{App, Arg};
+    use std::{fs::File, io::{Read, Write}};
 
-//    let _ = Command::new("go")
-//        .current_dir("wire-compatibility")
-//        .arg("run").arg("lnwire_gen.go").output().unwrap();
+    let matches = App::new("wire-compatibility")
+        .arg(
+            Arg::with_name("input-binary")
+                .long("input-binary")
+                .short("i")
+                .value_name("INPUT_BINARY")
+                .help("path to binary file containing input data")
+                .takes_value(true)
+                .default_value("/tmp/messages")
+        )
+        .arg(
+            Arg::with_name("output-binary")
+                .long("output-binary")
+                .short("b")
+                .value_name("INPUT_BINARY")
+                .help("path where to store binary file containing output data")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("output-text")
+                .long("output-text")
+                .short("t")
+                .value_name("INPUT_TEXT")
+                .help("path where to store text file containing output data")
+                .takes_value(true)
+                .default_value("/dev/stdout")
+        )
+        .get_matches();
 
     let data = {
-        let mut file = File::open("/tmp/messages").unwrap();
+        let mut file = File::open(matches.value_of("input-binary").unwrap())
+            .expect("missing input file");
         let mut v = Vec::new();
         file.read_to_end(&mut v).unwrap();
         v
     };
 
     let messages = de(data).unwrap();
-    for message in &messages {
-        println!("{:?}", message);
-    }
-    assert_eq!(de(ser(&messages).unwrap()).unwrap(), messages);
+    let _ = matches.value_of("output-binary")
+        .map(|output_binary_path| {
+            let mut file = File::create(output_binary_path).unwrap();
+            let data = ser(&messages).unwrap();
+            file.write_all(data.as_ref()).unwrap();
+        });
+    let _ = matches.value_of("output-text")
+        .map(|output_text_path| {
+            let mut file = File::create(output_text_path).unwrap();
+            // TODO(vlad): print is not pretty enough, use RON (https://github.com/ron-rs/ron)
+            file.write_fmt(format_args!("{:#?}", messages)).unwrap();
+        });
+
+    // assert_eq!(de(ser(&messages).unwrap()).unwrap(), messages);
 }
