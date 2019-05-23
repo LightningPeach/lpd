90d7bed1706fe6cf8a51acc309f645fac3d96f88
Vladislav Melnik <vladislav.melnik@bitfury.com>
2019-05-23 15:41:02 +0300

    pass channel update to the client through rpc interface

--- connection/src/lib.rs
+++ connection/src/lib.rs
@@ -5,5 +5,5 @@ mod address;
 mod ping;
 mod blockchain;
 
-pub use self::node::Node;
+pub use self::node::{Node, ChannelStatus};
 pub use self::address::{AbstractAddress, Command, ConnectionStream, Connection, TransportError};
